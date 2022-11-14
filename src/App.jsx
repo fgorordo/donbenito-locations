45260fc2789d384cd892f6bd11c20c68568ffec7
fgorordo <flgorordo@gmail.com>
2022-11-14 15:28:29 -0300

    end day 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import mainLogo from './assets/don-benito-logo.svg';
 import './main.css';
 import { AiOutlineHome } from 'react-icons/ai'
+import propsData from './data/props.json'
 function App() {
 
   return (
@@ -11,29 +12,19 @@ function App() {
       <p className="text-xl"><b>¿Necesitas un guia?</b><br />Selecciona donde quieres ir y te ayudamos.</p>
     </header>
     <main className="grid gap-2 grid-cols-2 px-8">
-      <div className="bg-green-600 hover:bg-green-400 transition-all w-[150px] h-[150px] rounded-lg flex flex-col justify-center items-center gap-2 drop-shadow-xl">
-        <AiOutlineHome className='text-white text-5xl'/>        
-        <p className="text-white text-xl font-bold text-center">UF 01</p>
-        <p className="text-white text-l font-semibold text-center">Pepito / Pepita </p>
-      </div>
-
-      <div className="bg-green-600 w-[150px] h-[150px] rounded-lg flex flex-col justify-center items-center gap-2">
-        <AiOutlineHome className='text-white text-5xl'/>        
-        <p className="text-white text-xl font-bold text-center">UF 01</p>
-        <p className="text-white text-l font-semibold text-center">Pepito / Pepita </p>
-      </div>
-
-      <div className="bg-green-600 w-[150px] h-[150px] rounded-lg flex flex-col justify-center items-center gap-2">
-        <AiOutlineHome className='text-white text-5xl'/>        
-        <p className="text-white text-xl font-bold text-center">UF 01</p>
-        <p className="text-white text-l font-semibold text-center">Pepito / Pepita </p>
-      </div>
-
-      <div className="bg-green-600 w-[150px] h-[150px] rounded-lg flex flex-col justify-center items-center gap-2">
-        <AiOutlineHome className='text-white text-5xl'/>        
-        <p className="text-white text-xl font-bold text-center">UF 01</p>
-        <p className="text-white text-l font-semibold text-center">Pepito / Pepita </p>
-      </div>
+      {
+        propsData.map(prop => {
+          return (
+            <a href={prop.locationLink}>
+              <div className="bg-green-600 hover:bg-green-400 transition-all w-[150px] h-[150px] rounded-lg flex flex-col justify-center items-center gap-2 drop-shadow-xl p-2">
+              <AiOutlineHome className='text-white text-5xl'/>        
+              <p className="text-white text-xl font-bold text-center">UF {prop.ufNumber}</p>
+              <p className="text-white text-sm font-semibold text-center">{prop.ufProp} </p>
+            </div>
+            </a>
+          )
+        })
+      }
     </main>
     </>
   )
