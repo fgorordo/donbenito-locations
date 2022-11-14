258badc5d8e2468f874b597c88f472d246332a49
fgorordo <flgorordo@gmail.com>
2022-11-14 14:17:03 -0300

    saving proto

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
+import mainLogo from './assets/don-benito-logo.svg';
+import './main.css';
+import { AiOutlineHome } from 'react-icons/ai'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+    <header className="text-center flex justify-center flex-col gap-4 pt-8 pb-8 container">
+      <img src={mainLogo} alt="Don Benito Logo" className="max-w-[340px] m-auto"/>
+      <h1 className="text-3xl font-bold">¡Bienvenido!</h1>
+      <p className="text-xl"><b>¿Necesitas un guia?</b><br />Selecciona donde quieres ir y te ayudamos.</p>
+    </header>
+    <main className="grid gap-2 grid-cols-2 px-8">
+      <div className="bg-green-600 hover:bg-green-400 transition-all w-[150px] h-[150px] rounded-lg flex flex-col justify-center items-center gap-2 drop-shadow-xl">
+        <AiOutlineHome className='text-white text-5xl'/>        
+        <p className="text-white text-xl font-bold text-center">UF 01</p>
+        <p className="text-white text-l font-semibold text-center">Pepito / Pepita </p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="bg-green-600 w-[150px] h-[150px] rounded-lg flex flex-col justify-center items-center gap-2">
+        <AiOutlineHome className='text-white text-5xl'/>        
+        <p className="text-white text-xl font-bold text-center">UF 01</p>
+        <p className="text-white text-l font-semibold text-center">Pepito / Pepita </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+
+      <div className="bg-green-600 w-[150px] h-[150px] rounded-lg flex flex-col justify-center items-center gap-2">
+        <AiOutlineHome className='text-white text-5xl'/>        
+        <p className="text-white text-xl font-bold text-center">UF 01</p>
+        <p className="text-white text-l font-semibold text-center">Pepito / Pepita </p>
+      </div>
+
+      <div className="bg-green-600 w-[150px] h-[150px] rounded-lg flex flex-col justify-center items-center gap-2">
+        <AiOutlineHome className='text-white text-5xl'/>        
+        <p className="text-white text-xl font-bold text-center">UF 01</p>
+        <p className="text-white text-l font-semibold text-center">Pepito / Pepita </p>
+      </div>
+    </main>
+    </>
   )
 }
 
